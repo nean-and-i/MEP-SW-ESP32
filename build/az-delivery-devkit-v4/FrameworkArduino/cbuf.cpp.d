39e3138e56a4db39ec4c305cb917ff796ddaef26
nean <nean.and.i@github.com>
2023-01-16 20:45:47 +0100

    convert to platformio W.I.P

---
+++ build/az-delivery-devkit-v4/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/az-delivery-devkit-v4/FrameworkArduino/cbuf.cpp.o: \
+ /Users/nean/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/nean/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ build/az-delivery-devkit-v4/libcc1/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/az-delivery-devkit-v4/libcc1/WebServer/detail/mimetable.cpp.o: \
+ /Users/nean/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ /Users/nean/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ /Users/nean/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
